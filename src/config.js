1a71946c988d232576882cc41f1237af0aa89447
Aaron Pennington <aaronpennington@Aarons-MacBook-Pro.local>
2020-01-13 19:26:09 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-west-2",
+      BUCKET: "nanowrit-notes-uploads"
+    },
+    apiGateway: {
+      REGION: "us-west-2",
+      URL: "https://oep9t9jqs7.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-west-2",
+      USER_POOL_ID: "us-west-2_bXf1J3vZf",
+      APP_CLIENT_ID: "3on5n5ocag0bep64h7rc23r6in",
+      IDENTITY_POOL_ID: "us-west-2:881e2366-6389-41ab-aa3c-fa9ff36df0ff"
+    }
+  };
