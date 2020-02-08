5a198681b6a0f101e0e50564e66f1aa9eea7e3cd
Aaron Pennington <aaronpennington@Aarons-MacBook-Pro.local>
2020-02-07 18:04:17 -0700

    added some simple branding for this use case project

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -55,8 +55,8 @@ export default function Home(props) {
   function renderLander() {
     return (
       <div className="lander">
-        <h1>Scratch</h1>
-        <p>A simple note taking app</p>
+        <h1>Scene Builder</h1>
+        <p>A simple scene building app</p>
         <div>
           <Link to="/login" className="btn btn-info btn-lg">
             Login
